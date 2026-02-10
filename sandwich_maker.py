75c4edb65850cc5bcf832db45aade40b921a830a
Sherelle McDaniel <sherelle.r.a.mcdaniel@gmail.com>
2026-02-10 11:46:49 -0500

    feat: added check resources and make sandwich functions in sandwich maker class

--- sandwich_maker.py
+++ sandwich_maker.py
@@ -5,7 +5,15 @@ class SandwichMaker:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        for ingredient in ingredients.keys() & self.machine_resources.keys(): # compares ingredient amt to resource amt
+            if ingredients[ingredient] > self.machine_resources[ingredient]:
+                print(f"Sorry, there is not enough {ingredient}.")
+                return False
+            else:
+                continue
+        return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+        for ingredient in order_ingredients.keys() & self.machine_resources.keys():
+            self.machine_resources[ingredient] -= order_ingredients[ingredient]
+        print(f"{sandwich_size} sandwich is ready. Bon appetit!")
